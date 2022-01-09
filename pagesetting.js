041d66bbc3dcee4de114b7dff05717dbf6ac393e
daddiboi <sahilpreet907@gamil.com>
2022-01-10 00:01:35 +0530

    fghj

--- pagesetting.js
+++ pagesetting.js
@@ -4,11 +4,11 @@ function showdiv1() {
     document.getElementById("div2").style.display="none";
 
 
-    $(window).scrollTop(position);
+  
   }
   function showdiv2() {
     document.getElementById("div1").style.display="none";
     document.getElementById("div2").style.display="block";
 
-    $(window).scrollTop(position);
+    
   }
